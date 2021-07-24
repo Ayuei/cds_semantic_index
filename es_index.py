d83ef33d1a214b49ac60372aa6e46d1f7fafde63
Ayuei <sefton_student@hotmail.com>
2021-07-24 13:56:57 +1000

    update: es_index.py

--- es_index.py
+++ es_index.py
@@ -4,9 +4,17 @@ import tqdm
 import numpy as np
 import torch
 import asyncio 
-import os
+import sentence_transformers
 import spacy
 from typing import List, Union
+import jsonlines
+
+INDEX_NAME = "clinical_test"
+DOCUMENT_FP = "es_index.json"
+MODEL_FP = "model/"
+
+es = elasticsearch.Elasticsearch()
+sentence_encoder = sentence_transformers.SentenceTransformer(MODEL_FP)
 
 best_fields = ["LocationCountries.Country", "BiospecRetention", "DetailedDescription.Textblock", "HasExpandedAccess",
                             "ConditionBrowse.MeshTerm", "RequiredHeader.LinkText", "WhyStopped", "BriefSummary.Textblock",
@@ -17,14 +25,14 @@ best_fields = ["LocationCountries.Country", "BiospecRetention", "DetailedDescrip
                             "Phase", "OverallContactBackup.LastName", "Acronym", "InterventionBrowse.MeshTerm", "RemovedCountries.Country"]
 
 
-async def embed_field(bc, field: Union[str,List[str]]=None):
+async def embed_field(field: Union[str,List[str]]=None):
     await asyncio.sleep(0.1)
 
     if field:
-        return torch.mean(bc.encode(field, numpy=False), axis=0)
+        return torch.mean(sentence_encoder.encode(field, numpy=False), axis=0)
 
     return [0]*768
-    
+
 
 def retrieve_field(document, field):
     fields = field.split(".")
@@ -65,15 +73,26 @@ async def encode_document(document):
         add_field(document, field, embedding)
 
 
-async def index_documents(parsed_ids, json_lines, index_name):
+async def index_documents(parsed_ids, document_itr, index_name):
     nlp = spacy.load("en_core_sci_sm", disable=['ner', 'tagger'])
     nlp.max_length = 2000000
 
-    for _, document in tqdm(json_lines):
+    for _, document in tqdm(document_itr):
         _id = document['_id']
+        
+        if _id in parsed_ids:
+            continue
+
         try:
             encode_document(document)
             es.update(index=index_name, id=_id, body=document)
         except:
             print(f"Cannot process doc {_id}")
             pass
+
+
+if __name__ == "__main__":
+    document_reader = jsonlines.open(DOCUMENT_FP)
+    parsed_ids = open("parsed_ids.txt", "r+").readlines()
+
+    asyncio.run(index_documents(parsed_ids, document_itr=document_reader, index_name=INDEX_NAME))
