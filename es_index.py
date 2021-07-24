dc33c7395d8828a37bbd4429618cc909062cbf75
Ayuei <sefton_student@hotmail.com>
2021-07-24 17:56:31 +1000

    Bug fixes

--- es_index.py
+++ es_index.py
@@ -3,6 +3,7 @@ import plac
 import tqdm
 import numpy as np
 import torch
+import torch.nn.functional as F
 import asyncio 
 import sentence_transformers
 import spacy
@@ -15,6 +16,8 @@ MODEL_FP = "model/"
 
 es = elasticsearch.Elasticsearch()
 sentence_encoder = sentence_transformers.SentenceTransformer(MODEL_FP)
+nlp = spacy.load("en_core_sci_sm", disable=['ner', 'tagger'])
+nlp.max_length = 2000000
 
 best_fields = ["LocationCountries.Country", "BiospecRetention", "DetailedDescription.Textblock", "HasExpandedAccess",
                             "ConditionBrowse.MeshTerm", "RequiredHeader.LinkText", "WhyStopped", "BriefSummary.Textblock",
@@ -25,11 +28,16 @@ best_fields = ["LocationCountries.Country", "BiospecRetention", "DetailedDescrip
                             "Phase", "OverallContactBackup.LastName", "Acronym", "InterventionBrowse.MeshTerm", "RemovedCountries.Country"]
 
 
-async def embed_field(field: Union[str,List[str]]=None):
+async def encode_field(field: Union[str,List[str]]=None):
     await asyncio.sleep(0.1)
 
+    embedding = sentence_encoder.encode(field, convert_to_tensors=True),
+
+    if len(embedding.size()) == 1:
+        embeddings = torch.unsqueeze(embedding)
+
     if field:
-        return torch.mean(sentence_encoder.encode(field, numpy=False), axis=0)
+        return F.norm(torch.mean(embeddings, axis=0), dim=0)
 
     return [0]*768
 
@@ -44,51 +52,45 @@ def retrieve_field(document, field):
 
 
 def add_field(document, field, value):
-    fields = field.split(".")
+    field = field.replace(".", '_')+'_Embedding'
 
-    temp = {}
-    orig = temp
-    i = 0
-
-    for field in fields:
-        if i == len(field) - 1:
-            temp[field] = value
-            document.update(orig)
-            break
-
-        temp[field] = {}
-        temp = temp[field]
-
-        i+=1
-
-    return document
+    document[field] = value
 
 
 async def encode_document(document):
     await asyncio.sleep(0.1)
 
+    update_document = {}
+
     for field in best_fields:
-        field = retrieve_field(document, field)
-        embedding = embed_field(field)
-        add_field(document, field, embedding)
+        embed_field = retrieve_field(document, field)
+
+        if field.endswith('Textblock'):
+            embed_field = [' '.join(sent.text.split()) for sent in nlp(doc['abstract']).sents if sent.text.strip()]
+
+        embedding = encode_field(embed_field)
+        add_field(update_document, field, embedding)
+
+    return update_document
 
 
 async def index_documents(parsed_ids, document_itr, index_name):
-    nlp = spacy.load("en_core_sci_sm", disable=['ner', 'tagger'])
-    nlp.max_length = 2000000
+    with open('parsed_ids.txt', 'a+') as parsed_writer:
+        for _, document in tqdm.tqdm(document_itr):
+            _id = document['_id']
 
-    for _, document in tqdm(document_itr):
-        _id = document['_id']
-        
-        if _id in parsed_ids:
-            continue
+            document = document['_source']
 
-        try:
-            encode_document(document)
-            es.update(index=index_name, id=_id, body=document)
-        except:
-            print(f"Cannot process doc {_id}")
-            pass
+            if _id in parsed_ids:
+                continue
+
+            try:
+                update_document = await encode_document(document)
+                es.update(index=index_name, id=_id, body=update_document)
+                parsed_writer.write(f"{_id}\n")
+            except:
+                print(f"Cannot process doc {_id}")
+                pass
 
 
 if __name__ == "__main__":
