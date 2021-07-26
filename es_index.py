d6e9ed6a5c3d79640cef3c91dbbaeebfcc423bc0
Ayuei <sefton_student@hotmail.com>
2021-07-26 17:33:23 +1000

    Cluster version

--- es_index.py
+++ es_index.py
@@ -8,12 +8,50 @@ import sentence_transformers
 import spacy
 from typing import List, Union
 import jsonlines
+import sys
+import os
+import json
+
+
+def reverse_readline(filename, buf_size=8192):
+    """A generator that returns the lines of a file in reverse order"""
+    with open(filename) as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        file_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(file_size, offset + buf_size)
+            fh.seek(file_size - offset)
+            buffer = fh.read(min(remaining_size, buf_size))
+            remaining_size -= buf_size
+            lines = buffer.split('\n')
+            # The first line of the buffer is probably not a complete line so
+            # we'll save it and append it to the last line of the next buffer
+            # we read
+            if segment is not None:
+                # If the previous chunk starts right from the beginning of line
+                # do not concat the segment to the last line of new chunk.
+                # Instead, yield the segment first
+                if buffer[-1] != '\n':
+                    lines[-1] += segment
+                else:
+                    yield segment
+            segment = lines[0]
+            for index in range(len(lines) - 1, 0, -1):
+                if lines[index]:
+                    yield lines[index]
+        # Don't yield None if the file was empty
+        if segment is not None:
+            yield segment
+
 
 INDEX_NAME = "test_trials"
-DOCUMENT_FP = "es_index.json"
 MODEL_FP = "model/"
+NUBMER = sys.argv[1]
+DOCUMENT_FP = "es_index.jsona"+NUMBER
 
-es = elasticsearch.AsyncElasticsearch([{'host': "192.168.20.29"}])
+es = elasticsearch.AsyncElasticsearch([{'host': ""}])
 sentence_encoder = sentence_transformers.SentenceTransformer(MODEL_FP)
 nlp = spacy.load("en_core_sci_sm")
 nlp.max_length = 2000000
@@ -93,22 +131,27 @@ async def encode_document(document):
 
 async def index_documents(parsed_ids, document_itr, index_name):
     await update_mappings()
-    with open('parsed_ids.txt', 'a+') as parsed_writer:
+    with open('parsed_ids.txt'+NUMBER, 'a+') as parsed_writer:
         for document in tqdm.tqdm(document_itr, total=2.03e5):
+            document = json.loads(document)
             _id = document['_id']
 
-            document = document['_source']
+            try:
+                document = document['_source']
 
-            if _id in parsed_ids:
-                continue
+                if _id in parsed_ids:
+                    print(f'Skipping {_id}')
+                    continue
 
-            update_document = await encode_document(document)
-            await es.update(index=index_name, id=_id, body={'doc': update_document})
-            parsed_writer.write(f"{_id}\n")
+                update_document = await encode_document(document)
+                await es.update(index=index_name, id=_id, body={'doc': update_document})
+                parsed_writer.write(f"{_id}\n")
+            except Exception as e:
+                print(f"Cannot parse {_id}: {e}")
 
 
 if __name__ == "__main__":
-    document_reader = jsonlines.open(DOCUMENT_FP)
-    parsed_ids = open("parsed_ids.txt", "r+").readlines()
+    document_reader = reverse_readline(DOCUMENT_FP)
+    parsed_ids = open("parsed_ids.txt"+NUMBER, "r+").readlines()
 
     asyncio.run(index_documents(parsed_ids, document_itr=document_reader, index_name=INDEX_NAME))
